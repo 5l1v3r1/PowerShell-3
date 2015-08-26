1ec11d5523c84ce001d5e0a33cdcb2ad89cf3c9c
Vance Courtney <vancecourtney5@gmail.com>
2015-08-25 22:12:56 -0400

    Monitor Download Folder
    - Perform MD5 and SHA256
    - Check VirusTotal to Confrim
    - Pop-up notification

---
+++ download_monitor.ps1
@@ -0,0 +1,21 @@
+﻿# Figure out adding -Action for Register-WmiEvent
+
+$query = @" 
+ Select * from __InstanceCreationEvent within 5  
+ where targetInstance isa 'Cim_DirectoryContainsFile'  
+ and targetInstance.GroupComponent = 'Win32_Directory.Name="c:\\\\test"' 
+"@ 
+Register-WmiEvent -Query $query -SourceIdentifier "MonitorFiles" -Action 
+$fileEvent = Wait-Event -SourceIdentifier "MonitorFiles" 
+$name = ($fileEvent.SourceEventArgs.NewEvent.TargetInstance.PartComponent).split('"')[1]
+$md5 = (get-filehash $name -Algorithm MD5).hash
+$sha256 = (get-filehash $name -Algorithm SHA256).hash
+
+$wscript = New-Object -ComObject Wscript.Shell
+$wscript.Popup("Downloaded File: $name MD5: $md5 SHA256: $sha256",0,"Done",0x1)
+
+
+
+Get-EventSubscriber | Unregister-Event 
+Get-Event | Remove-Event 
+
